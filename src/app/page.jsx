77648fa2c7675482f1b5594f29425355478c7de6
Ajay Juneja <ajayjuneja0507@gmail.com>
2026-09-04 01:38:11 +0530

    Add CoreValues component matching reference video animation with GSAP Flip, 3D isometric cards, and carousel controls

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import WhyChooseUs from "../components/WhyChooseUs";
 import Services from "../components/Services";
+import CoreValues from "../components/CoreValues";
 import Specialists from "../components/Specialists";
 import AdvancedTechnology from "../components/AdvancedTechnology";
 import BookAppointment from "../components/BookAppointment";
@@ -16,6 +17,7 @@ export default function Home() {
       <Hero />
       <WhyChooseUs />
       <Services />
+      <CoreValues />
       <Specialists />
       <AdvancedTechnology />
       <BookAppointment />
